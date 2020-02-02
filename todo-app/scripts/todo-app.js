ef6f3499bb59c5400b7a7304fa151476ba3bc270
unknown <joel@joelgonzaga.com>
2020-02-01 17:03:03 -0800

    set input of TODO to sanitize and console log.

--- todo-app/scripts/todo-app.js
+++ todo-app/scripts/todo-app.js
@@ -15,15 +15,21 @@ document.querySelector('#search-text').addEventListener('input', (e) => {
 })
 
 document.querySelector('#new-todo').addEventListener('submit', (e) => {
+    
     e.preventDefault()
-    todos.push({
-        id: uuidv4(),
-        text: e.target.elements.text.value,
-        completed: false
-    })
-    saveTodos(todos)
-    renderTodos(todos, filters)
-    e.target.elements.text.value = ''
+    console.log(e.target.elements.text.value)
+    const text = e.target.elements.text.value
+    console.log(text)
+    if (text.length > 0){
+        todos.push({
+            id: uuidv4(),
+            text: e.target.elements.text.value,
+            completed: false
+        })
+        saveTodos(todos)
+        renderTodos(todos, filters)
+        e.target.elements.text.value = ''
+    }
 })
 
 document.querySelector('#hide-completed').addEventListener('change', (e) => {
